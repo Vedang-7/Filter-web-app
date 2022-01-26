b819f44005cfa3ae60ad0ba0795727c2109686ae
Vedang-7 <86293705+Vedang-7@users.noreply.github.com>
2022-01-26 12:40:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+}
+function setup(){
+   canvas= createCanvas(350, 400);
+   canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('my_filter_image.png');
+}
